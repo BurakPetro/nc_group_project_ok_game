0809477799cc1b2e2fc035d4aae5d115608a07a4
ahmedsatti101 <ahmedysatti@gmail.com>
2024-02-22 12:32:03 +0000

    Created instructions page and added a button for users to switch between welcome and instructions pages

--- client/src/components/Instructions/Instructions.jsx
+++ client/src/components/Instructions/Instructions.jsx
@@ -1,12 +1,39 @@
-import Button from "react-bootstrap/Button";
 import "./Instructions.css";
 
 const Instructions = () => {
   return (
     <>
-      <Button variant="light" id="back-button" href="/">
+      <button
+        id="back-button"
+        type="button"
+        onClick={() => {
+          window.location.href = '/Welcome'}}
+      >
         Back
-      </Button>
+      </button>
+      <section>
+        <article>
+          <h2>How to play OK Play</h2>
+          <p>
+            Your mission is to get five tiles in a row either vertically or
+            horizontally.
+          </p>
+          <p>You can play the game with two players and up to four players.</p>
+          <p>The first player plays their tile in the middle.</p>
+          <p>
+            The next player can <b>only</b> lay their tile on any flat edge of
+            the middle tile.
+          </p>
+          <p>
+            This continues like this with players trying to win the game with
+            five tiles in a row.
+          </p>
+          <p>
+            One last tip! Sometimes you may have to work together with others to
+            stop others from stealing a sneaky win!
+          </p>
+        </article>
+      </section>
     </>
   );
 };
--- client/src/components/WelcomePage.jsx
+++ client/src/components/WelcomePage.jsx
@@ -1,10 +1,26 @@
-import GameSettings from './GameSettings.jsx';
+import GameSettings from "./GameSettings.jsx";
 
 const WelcomePage = () => {
   return (
     <>
       <header>Ok Game</header>
       <div>Login</div>
+      <button
+        onClick={() => {
+          window.location.href = "/instructions";
+        }}
+        style={{
+          display: "flex",
+          position: "absolute",
+          top: "0%",
+          right: "0%",
+          marginTop: "1%",
+          marginRight: "1%",
+        }}
+        onMouseOver='test'
+      >
+        ?
+      </button>
       <GameSettings />
 
       <div>Chat Box</div>
