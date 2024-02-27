da4722a63b252124a9d7cd12ac22a28baca0b27a
pitburak@gmail.com <pit@MSI.localdomain>
2024-02-27 18:24:58 +0000

    Progect info page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import Instructions from "./components/Instructions";
 import WelcomePage from "./components/WelcomePage";
 import { Routes, Route } from "react-router-dom";
+import ProgectInfo from "./components/ProgectInfo";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/instructions" element={<Instructions />}></Route>
+        <Route path="/progect-info" element={<ProgectInfo />} />
+        <Route path="/instructions" element={<Instructions />} />
       </Routes>
     </>
   );
